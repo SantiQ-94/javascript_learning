c6f69bd79c2c86c85abcc38ff214aecfad034f8e
Santiago Quiroga Turdera <san.quit@hotmail.com>
2018-08-22 10:29:57 -0400

    not a big deal actually

---
+++ chapter_08.js
@@ -0,0 +1,16 @@
+function parseINI(string) {
+  let result = {};  //empty object that will hold the result.
+  let section = {};
+  string.split(/\r?\n/).forEach(line => {
+    let match;
+    if (match = line.match(/^(\w+)=(.*)$/)) {
+      section[match[1]] = match[2]; 
+    } else if (match = line.match(/^\[(.*)\]$/)) {
+      section = result[match[1]] = {}; 
+    } else if (!/^\s*(;.*)?$/.test(line)) {
+      throw new Error("Line '" + line + "' is not valid.");
+    }
+  });
+  return result;  
+}
+
